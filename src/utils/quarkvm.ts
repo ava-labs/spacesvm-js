8ece43daf65b4f0eb82632734f948eedbff8b5ab
Connor Chevli <connorchev@gmail.com>
2022-01-11 20:12:28 -0500

    feat: make quarkvm lib

---
+++ src/utils/quarkvm.ts
@@ -0,0 +1,31 @@
+export const mine = () => {}
+
+export const setValue = (key) => {}
+
+export const checkKeyClaimed = async (key) => {
+	const response = await fetch(
+		'http://3.80.126.27:37549/ext/bc/PWapoqFxsYJosghkRx5eBVZizvp9fPyKPGvdTBkoZYNdKj8Au/public',
+		{
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		},
+	)
+
+	const reader = response.body?.getReader()
+
+	const data = await reader?.read()
+	console.log(`data`, data)
+}
+
+// curl --location --request POST 'http://3.80.126.27:37549/ext/bc/PWapoqFxsYJosghkRx5eBVZizvp9fPyKPGvdTBkoZYNdKj8Au/public' \
+// --header 'Content-Type: application/json' \
+// --data-raw '{
+//     "jsonrpc": "2.0",
+//     "method": "quarkvm.prefixInfo",
+//     "params":{
+//         "prefix":"cGF0cmljay5hdmF4"
+//     },
+//     "id": 1
+// }'
