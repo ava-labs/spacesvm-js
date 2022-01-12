d339e7040fa479d2b813d47fbd0ccfe43210d0bb
Cohan Carpentier <cohan.carpentier@gmail.com>
2022-01-12 11:28:17 -0500

    fix: change url

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,7 +2,7 @@
 export const THEME_LOCAL_STORAGE_KEY = 'quark-theme'
 
 // api
-export const API_DOMAIN = 'http://3.80.126.27:32851/ext/bc/2F2ayy26TEJzwYn9zJhp7196VvBe4UaAAGEErbjPmsFnui4Zy2'
+export const API_DOMAIN = 'https://memeshowdown.com/ext/bc/2F2ayy26TEJzwYn9zJhp7196VvBe4UaAAGEErbjPmsFnui4Zy2'
 
 // Overview
 export const APP_NAME = 'Quark'
