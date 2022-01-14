49d522701f1b8390c5f3c2d7671e1688143246d7
Connor Chevli <connorchev@gmail.com>
2022-01-14 14:56:48 -0500

    feat: fill with sample button

--- src/pages/CustomSignature/CustomSignature.tsx
+++ src/pages/CustomSignature/CustomSignature.tsx
@@ -1,13 +1,15 @@
-import { Button, Card, Grid, styled, TextareaAutosize, Typography } from '@mui/material'
+import { Button, Card, Grid, Link, styled, TextareaAutosize, Typography } from '@mui/material'
 
 import { Page } from '@/components/Page'
 import { PageTitle } from '@/components/PageTitle'
 import { signWithMetaMask } from '@/utils/metamask'
 
 const JsonTextArea = styled(TextareaAutosize)`
-	min-height: 800px;
 	width: 100%;
 	max-width: 100%;
+	min-width: 100%;
+	border: 0;
+	outline: none;
 `
 
 const jsonPlaceholder = `[
@@ -30,7 +32,7 @@ const jsonPlaceholder = `[
 `
 
 export const CustomSignature = () => {
-	const [json, setJson] = useState<string | null>(null)
+	const [json, setJson] = useState<string>('')
 	const [signature, setSignature] = useState<string | null>(null)
 	const [error, setError] = useState<string | null>(null)
 
@@ -58,10 +60,26 @@ export const CustomSignature = () => {
 			<PageTitle variant="h3" sx={{ mt: 3 }}>
 				Hi, Patrick!
 			</PageTitle>
-			<Grid container sx={{ width: '100%' }} spacing={1}>
+			<Grid container sx={{ width: '100%', height: 800 }} spacing={1}>
 				<Grid item md={5} xs={12}>
-					<Typography variant="h6">Input:</Typography>
-					<JsonTextArea onChange={(e) => setJson(e.target.value)} placeholder={jsonPlaceholder} />
+					<Grid container justifyContent="space-between" alignItems="end">
+						<Grid item>
+							<Typography variant="h6">Input:</Typography>
+						</Grid>
+						<Grid item>
+							<Button variant="text" sx={{ py: 0 }} onClick={() => setJson(jsonPlaceholder)}>
+								Fill with sample.
+							</Button>
+						</Grid>
+					</Grid>
+					<Card sx={{ p: 2, height: '100%' }}>
+						<JsonTextArea
+							onChange={(e) => setJson(e.target.value)}
+							value={json}
+							placeholder={jsonPlaceholder}
+							sx={{ borderRadius: 2, padding: 2 }}
+						/>
+					</Card>
 				</Grid>
 				<Grid item md={2} xs={12}>
 					<Button
@@ -78,7 +96,7 @@ export const CustomSignature = () => {
 				</Grid>
 				<Grid item md={5} xs={12}>
 					<Typography variant="h6">Signature:</Typography>
-					<Card sx={{ height: 800, width: '100%', p: 2, maxWidth: 900 }}>
+					<Card sx={{ height: '100%', width: '100%', p: 2, maxWidth: 900 }}>
 						{error ? (
 							<Typography color="red" fontFamily="monospace">
 								{error}
