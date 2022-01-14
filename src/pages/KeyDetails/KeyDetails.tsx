9cd70e7cc1b7cb24e856c09a966a221d6cf0d42d
Connor Chevli <connorchev@gmail.com>
2022-01-14 01:07:31 -0500

    feat: basic keydetails page

--- src/pages/KeyDetails/KeyDetails.tsx
+++ src/pages/KeyDetails/KeyDetails.tsx
@@ -1,10 +1,44 @@
+import { useParams } from 'react-router-dom'
+import { Typography, useTheme } from '@mui/material'
+
 import { Page } from '@/components/Page'
+import { getQuarkValue } from '@/utils/quarkvm'
 
 export const KeyDetails = () => {
-	const test = true
-	useEffect(() => {
-		// getQuarkValue()
-	}, [])
+	const { spaceId, key } = useParams()
+	const theme = useTheme()
+	const [value, setValue] = useState<string | null>(null)
 
-	return <Page></Page>
+	useEffect(() => {
+		const getValue = async () => {
+			if (!spaceId || !key) return
+			const value = await getQuarkValue(spaceId, key)
+			setValue(value)
+		}
+		getValue()
+	}, [spaceId, key])
+
+	return (
+		<Page>
+			{value && (
+				<Typography
+					variant="h1"
+					sx={{
+						mt: 8,
+						textAlign: 'center',
+						backgroundSize: '400% 100%',
+						backgroundClip: 'text',
+						textFillColor: 'transparent',
+						backgroundColor: theme.palette.mode === 'dark' ? 'white' : 'unset',
+						animation: 'hue 5s infinite alternate',
+						caretColor: '#523df1',
+						backgroundImage:
+							'linear-gradient(60deg,rgba(239,0,143,.5),rgba(110,195,244,.5),rgba(112,56,255,.5),rgba(255,186,39,.5))',
+					}}
+				>
+					{value}
+				</Typography>
+			)}
+		</Page>
+	)
 }
