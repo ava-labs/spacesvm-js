1c377042d1a5ba1c522d1634bf9991fb0e6cc52e
Cohan Carpentier <cohan.carpentier@gmail.com>
2022-01-17 13:55:53 -0500

    fix: build

--- src/components/LinkPreview/LinkPreview.tsx
+++ src/components/LinkPreview/LinkPreview.tsx
@@ -1,19 +1,13 @@
-import axios from 'axios'
+// @ts-ignore
 import getMeta from 'lets-get-meta'
 
 export const LinkPreview = memo(({ url, render }: any) => {
 	const [loading, setLoading] = useState<boolean>(true)
 	const [preview, setPreviewData] = useState<any>({})
-	const api = `https://cors-proxy.htmldriven.com/?url=${url}`
 
 	useEffect(() => {
-		async function fetchData() {
+		const fetchData = async () => {
 			setLoading(true)
-			console.log('YOOO')
-			/*const response = await axios.get(api).then((res) => {
-				console.log(res)
-				return res
-			})*/
 
 			fetch(`https://api.allorigins.win/get?url=${encodeURIComponent(url)}`)
 				.then((response) => {
@@ -21,7 +15,6 @@ export const LinkPreview = memo(({ url, render }: any) => {
 					throw new Error('Network response was not ok.')
 				})
 				.then((data) => {
-					console.log(getMeta(data.contents))
 					setPreviewData(getMeta(data.contents))
 					setLoading(false)
 				})
