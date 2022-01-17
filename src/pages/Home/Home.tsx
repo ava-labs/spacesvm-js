9d74c066d4d91290aef055081c0c0e543bb4c925
Connor Chevli <connorchev@gmail.com>
2022-01-17 13:10:29 -0500

    feat: show homepage on mobile

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -178,251 +178,245 @@ export const Home = memo(() => {
 				</DialogContent>
 			</Dialog>
 
-			{isMobile ? (
-				<Alert severity="error" sx={{ m: 2 }}>
-					This website can only be used from a web browser, due to the MetaMask integration.
-				</Alert>
-			) : (
-				<form onSubmit={handleSubmit} autoComplete="off">
-					<PageTitle align="center" lineHeight={1}>
-						Claim your space
-					</PageTitle>
-					<PageSubtitle align="center">Needs to be unique and lowercase.</PageSubtitle>
+			<form onSubmit={handleSubmit} autoComplete="off">
+				<PageTitle align="center" lineHeight={1}>
+					Claim your space
+				</PageTitle>
+				<PageSubtitle align="center">Needs to be unique and lowercase.</PageSubtitle>
 
-					<Grid container spacing={4} flexDirection="column" alignItems="center">
-						<Grid item>
-							<TypewrittingInput waitBeforeDeleteMs={2000} strings={USERNAMES}>
-								{({ currentText }) => (
-									<TextField
-										disabled={available && verified}
-										value={username}
-										onChange={(e) => {
-											if (e.target.value === '' || USERNAME_REGEX.test(e.target.value)) {
-												setVerified(false)
-												setUsername(e.target.value.toLowerCase())
-											}
-										}}
-										placeholder={currentText.toLowerCase()}
-										fullWidth
-										autoFocus
-										InputProps={{
-											sx: {
-												fontSize: {
-													xs: 24,
-													sm: 42,
-													md: 80,
-												},
-												fontWeight: 900,
-												fontFamily: 'DM Serif Display',
+				<Grid container spacing={4} flexDirection="column" alignItems="center">
+					<Grid item>
+						<TypewrittingInput waitBeforeDeleteMs={2000} strings={USERNAMES}>
+							{({ currentText }) => (
+								<TextField
+									disabled={available && verified}
+									value={username}
+									onChange={(e) => {
+										if (e.target.value === '' || USERNAME_REGEX.test(e.target.value)) {
+											setVerified(false)
+											setUsername(e.target.value.toLowerCase())
+										}
+									}}
+									placeholder={currentText.toLowerCase()}
+									fullWidth
+									autoFocus
+									InputProps={{
+										sx: {
+											fontSize: {
+												xs: 24,
+												sm: 42,
+												md: 80,
 											},
-											startAdornment: (
-												<InputAdornment sx={{ marginRight: 4 }} position="start">
-													<IoSearch color="grey" />
-												</InputAdornment>
-											),
-											endAdornment: (
-												<InputAdornment position="end" sx={{ width: 80, height: 80 }}>
-													{verified && available && (
-														<Fade in>
-															<Tooltip placement="top" title="Clear">
-																<IconButton
-																	size="large"
-																	onClick={() => {
-																		setVerified(false)
-																		setUsername('')
-																	}}
-																>
-																	<IoClose size={48} color="grey" />
-																</IconButton>
-															</Tooltip>
-														</Fade>
-													)}
-												</InputAdornment>
-											),
-										}}
-										inputProps={{
-											maxLength: 255,
-											spellCheck: 'false',
+											fontWeight: 900,
+											fontFamily: 'DM Serif Display',
+										},
+										startAdornment: (
+											<InputAdornment sx={{ marginRight: 4 }} position="start">
+												<IoSearch color="grey" />
+											</InputAdornment>
+										),
+										endAdornment: (
+											<InputAdornment position="end" sx={{ width: 80, height: 80 }}>
+												{verified && available && (
+													<Fade in>
+														<Tooltip placement="top" title="Clear">
+															<IconButton
+																size="large"
+																onClick={() => {
+																	setVerified(false)
+																	setUsername('')
+																}}
+															>
+																<IoClose size={48} color="grey" />
+															</IconButton>
+														</Tooltip>
+													</Fade>
+												)}
+											</InputAdornment>
+										),
+									}}
+									inputProps={{
+										maxLength: 255,
+										spellCheck: 'false',
+									}}
+								/>
+							)}
+						</TypewrittingInput>
+					</Grid>
+					<Grid
+						item
+						container
+						justifyContent="center"
+						alignItems="center"
+						spacing={4}
+						sx={{
+							flexWrap: {
+								xs: 'wrap',
+								md: 'nowrap',
+							},
+						}}
+					>
+						<Grid item>
+							{verified && available ? (
+								<ClaimButton
+									onClick={onClaim}
+									fullWidth
+									disabled={claiming || username.length === 0 || waitingForMetaMask}
+									variant="contained"
+									size="large"
+								>
+									{claiming && <CircularProgress color="inherit" size={32} sx={{ mr: 2 }} />}
+									{waitingForMetaMask ? (
+										<Fade in={waitingForMetaMask}>
+											<img src={MetaMaskFoxLogo} alt="metamask-fox" style={{ height: '100%' }} />
+										</Fade>
+									) : claiming ? (
+										'Claiming...'
+									) : (
+										'Claim'
+									)}
+								</ClaimButton>
+							) : (
+								<VerifyButton
+									type="submit"
+									onClick={() => (verified ? navigate(`/spaces/${username}/`) : onVerify())}
+									fullWidth
+									disabled={username.length === 0}
+									variant="contained"
+									size="large"
+								>
+									{verified ? 'View space' : 'Check availability'}
+								</VerifyButton>
+							)}
+						</Grid>
+
+						{verified && costEstimate && available && (
+							<>
+								<Grid
+									item
+									sx={{
+										height: '100%',
+										display: {
+											xs: 'none',
+											md: 'flex',
+										},
+									}}
+								>
+									<Divider
+										flexItem
+										orientation="vertical"
+										sx={{
+											height: 60,
 										}}
 									/>
-								)}
-							</TypewrittingInput>
-						</Grid>
-						<Grid
-							item
-							container
-							justifyContent="center"
-							alignItems="center"
-							spacing={4}
-							sx={{
-								flexWrap: {
-									xs: 'wrap',
-									md: 'nowrap',
-								},
-							}}
-						>
-							<Grid item>
-								{verified && available ? (
-									<ClaimButton
-										onClick={onClaim}
-										fullWidth
-										disabled={claiming || username.length === 0 || waitingForMetaMask}
-										variant="contained"
-										size="large"
-									>
-										{claiming && <CircularProgress color="inherit" size={32} sx={{ mr: 2 }} />}
-										{waitingForMetaMask ? (
-											<Fade in={waitingForMetaMask}>
-												<img src={MetaMaskFoxLogo} alt="metamask-fox" style={{ height: '100%' }} />
-											</Fade>
-										) : claiming ? (
-											'Claiming...'
-										) : (
-											'Claim'
-										)}
-									</ClaimButton>
-								) : (
-									<VerifyButton
-										type="submit"
-										onClick={() => (verified ? navigate(`/spaces/${username}/`) : onVerify())}
-										fullWidth
-										disabled={username.length === 0}
-										variant="contained"
-										size="large"
-									>
-										{verified ? 'View space' : 'Check availability'}
-									</VerifyButton>
-								)}
-							</Grid>
-
-							{verified && costEstimate && available && (
-								<>
-									<Grid
-										item
-										sx={{
-											height: '100%',
-											display: {
-												xs: 'none',
-												md: 'flex',
-											},
-										}}
-									>
-										<Divider
-											flexItem
-											orientation="vertical"
+								</Grid>
+								<Grid item container wrap="nowrap" spacing={4} alignItems="center" flexBasis="content">
+									<Grid item>
+										<Box
+											position="relative"
+											width={144}
+											height={72}
+											display="flex"
+											alignItems="center"
+											justifyContent="center"
 											sx={{
-												height: 60,
+												ml: 2,
+												'&:before': {
+													position: 'absolute',
+													content: "''",
+													background: 'linear-gradient(100deg,#aa039f,#ed014d,#f67916)',
+													top: '50%',
+													left: '50%',
+													width: costEstimate ? '115%' : '100%',
+													height: costEstimate ? '125%' : '100%',
+													transform: 'translate3d(-50%,-50%,0)',
+													filter: `blur(8px) ${!costEstimate ? 'grayscale(0.92) opacity(0.5)' : ''}`,
+													transitionProperty: 'width, height, filter',
+													transitionDuration: '0.2s',
+													transitionTimingFunction: 'ease',
+													borderRadius: 3,
+													zIndex: 1,
+												},
+												'&:after': {
+													content: "''",
+													top: 0,
+													right: 0,
+													bottom: 0,
+													left: 0,
+													zIndex: 2,
+													position: 'absolute',
+													borderRadius: 3,
+													backgroundColor: (theme) => theme.customPalette.customBackground,
+												},
 											}}
-										/>
-									</Grid>
-									<Grid item container wrap="nowrap" spacing={4} alignItems="center" flexBasis="content">
-										<Grid item>
-											<Box
-												position="relative"
-												width={144}
-												height={72}
-												display="flex"
-												alignItems="center"
-												justifyContent="center"
-												sx={{
-													ml: 2,
-													'&:before': {
-														position: 'absolute',
-														content: "''",
-														background: 'linear-gradient(100deg,#aa039f,#ed014d,#f67916)',
-														top: '50%',
-														left: '50%',
-														width: costEstimate ? '115%' : '100%',
-														height: costEstimate ? '125%' : '100%',
-														transform: 'translate3d(-50%,-50%,0)',
-														filter: `blur(8px) ${!costEstimate ? 'grayscale(0.92) opacity(0.5)' : ''}`,
-														transitionProperty: 'width, height, filter',
-														transitionDuration: '0.2s',
-														transitionTimingFunction: 'ease',
-														borderRadius: 3,
-														zIndex: 1,
-													},
-													'&:after': {
-														content: "''",
-														top: 0,
-														right: 0,
-														bottom: 0,
-														left: 0,
-														zIndex: 2,
-														position: 'absolute',
-														borderRadius: 3,
-														backgroundColor: (theme) => theme.customPalette.customBackground,
-													},
+										>
+											<Typography
+												noWrap
+												variant="h6"
+												align="center"
+												style={{
+													zIndex: 3,
+													marginTop: 4,
+													fontSize: costEstimate ? '1.5rem' : 36,
+													position: 'relative',
 												}}
 											>
-												<Typography
-													noWrap
-													variant="h6"
-													align="center"
-													style={{
-														zIndex: 3,
-														marginTop: 4,
-														fontSize: costEstimate ? '1.5rem' : 36,
-														position: 'relative',
-													}}
-												>
-													{new Intl.NumberFormat('en-US').format(costEstimate)}
-												</Typography>
-											</Box>
-										</Grid>
-										<Grid item>
-											<Typography variant="h6" gutterBottom>
-												Cost{' '}
-												<Typography component="span" color="textSecondary">
-													(SPC)
-												</Typography>
+												{new Intl.NumberFormat('en-US').format(costEstimate)}
 											</Typography>
-											<Typography variant="body2" color="textSecondary">
-												The{' '}
-												<Typography component="b" fontWeight={900} variant="body2" color="textPrimary">
-													shorter
-												</Typography>{' '}
-												the username,
-												<br />
-												the{' '}
-												<Typography component="b" fontWeight={900} variant="body2" color="textPrimary">
-													more
-												</Typography>{' '}
-												it will cost to claim.
-											</Typography>
-										</Grid>
+										</Box>
 									</Grid>
-								</>
-							)}
-						</Grid>
+									<Grid item>
+										<Typography variant="h6" gutterBottom>
+											Cost{' '}
+											<Typography component="span" color="textSecondary">
+												(SPC)
+											</Typography>
+										</Typography>
+										<Typography variant="body2" color="textSecondary">
+											The{' '}
+											<Typography component="b" fontWeight={900} variant="body2" color="textPrimary">
+												shorter
+											</Typography>{' '}
+											the username,
+											<br />
+											the{' '}
+											<Typography component="b" fontWeight={900} variant="body2" color="textPrimary">
+												more
+											</Typography>{' '}
+											it will cost to claim.
+										</Typography>
+									</Grid>
+								</Grid>
+							</>
+						)}
 					</Grid>
+				</Grid>
 
-					<Grow in={verified}>
-						<div>
-							{available ? (
-								<Alert
-									icon={
-										<IoCheckmarkCircle style={{ position: 'relative', top: 2, color: theme.palette.success.light }} />
-									}
-									severity="success"
-									sx={{ m: 'auto', mt: 4, mb: 0, maxWidth: 480, justifyContent: 'center' }}
-								>
-									<Typography>This space is available!</Typography>
-								</Alert>
-							) : (
-								<Typography
-									align="center"
-									sx={{ m: 'auto', mt: 4, mb: 0, maxWidth: 860, display: 'block' }}
-									gutterBottom
-									color="error"
-								>
-									This space is already taken
-								</Typography>
-							)}
-						</div>
-					</Grow>
-				</form>
-			)}
+				<Grow in={verified}>
+					<div>
+						{available ? (
+							<Alert
+								icon={
+									<IoCheckmarkCircle style={{ position: 'relative', top: 2, color: theme.palette.success.light }} />
+								}
+								severity="success"
+								sx={{ m: 'auto', mt: 4, mb: 0, maxWidth: 480, justifyContent: 'center' }}
+							>
+								<Typography>This space is available!</Typography>
+							</Alert>
+						) : (
+							<Typography
+								align="center"
+								sx={{ m: 'auto', mt: 4, mb: 0, maxWidth: 860, display: 'block' }}
+								gutterBottom
+								color="error"
+							>
+								This space is already taken
+							</Typography>
+						)}
+					</div>
+				</Grow>
+			</form>
 		</Page>
 	)
 })
