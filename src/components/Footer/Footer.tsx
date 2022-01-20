184eecf1f48c73c1fa7b0463ad7185cc10e59a9d
Connor Chevli <connorchev@gmail.com>
2022-01-19 23:14:54 -0500

    feat: wip footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,4 +1,5 @@
 import { IoLogoGithub, IoOpenOutline } from 'react-icons/io5'
+import { Link as ReactRouterLink } from 'react-router-dom'
 import {
 	Box,
 	Button,
@@ -14,6 +15,7 @@ import {
 } from '@mui/material'
 
 import AvalancheLogo from '@/assets/avax-logo-official.svg'
+import Logo from '@/assets/spaces-logo.png'
 import { APP_NAME, CHAIN_ID_URL, SUBNET_ID_URL } from '@/constants'
 import { obfuscateAddress } from '@/utils/obfuscateAddress'
 import { getNetworks } from '@/utils/spacesVM'
@@ -44,6 +46,127 @@ export const Footer = memo(() => {
 	return (
 		<Box pb={isMobile ? 6 : 4}>
 			<Divider sx={{ my: 4 }} />
+			<Grid container spacing={2}>
+				<Grid item container direction="column" alignItems="flex-end" xs={6}>
+					<Grid item>
+						<ReactRouterLink
+							to="/"
+							style={{
+								textDecoration: 'none',
+								display: 'flex',
+								alignItems: 'center',
+							}}
+						>
+							<img src={Logo} width={isMobile ? 24 : 32} height={isMobile ? 24 : 32} alt={`${APP_NAME} Logo`} />
+							<Typography
+								variant="h4"
+								component="div"
+								color="textPrimary"
+								sx={{ fontFamily: 'DM Serif Display', ml: 2, letterSpacing: 6 }}
+							>
+								{APP_NAME}
+							</Typography>
+						</ReactRouterLink>
+					</Grid>
+					<Grid item>
+						<Typography component="footer" variant="body2" sx={{ width: '100%', opacity: 0.5, py: 1 }} align="right">
+							{`© ${new Date().getFullYear()} ${APP_NAME}`}
+						</Typography>
+					</Grid>
+					<Grid item>
+						<Typography component="footer" variant="body2" sx={{ width: '100%', opacity: 0.5, py: 1 }} align="right">
+							<Link color="inherit" href="https://spacesvm.xyz/" rel="noopener noreferrer" target="_blank">
+								Powered by the SpacesVM Avalanche Subnet
+							</Link>
+						</Typography>
+					</Grid>
+					<Grid item>
+						<Typography component="footer" variant="body2" sx={{ width: '100%', opacity: 0.5, py: 1 }} align="right">
+							<Link color="inherit" href="https://dribbble.com/dmitrymoi/" rel="noopener noreferrer" target="_blank">
+								Illustrations by Dmitry Moiseenko
+							</Link>
+						</Typography>
+					</Grid>
+				</Grid>
+				<Grid item>
+					<Divider orientation="vertical" />
+				</Grid>
+				<Grid item>
+					<Grid container direction="column" spacing={1}>
+						<Grid item>
+							<Typography component="span" variant="body2" color="textSecondary">
+								See us on Avascan!&nbsp;
+							</Typography>
+							<Link>Our Chain</Link>{' '}
+							<Typography component="span" variant="body2" color="textSecondary">
+								and
+							</Typography>{' '}
+							<Link>our subnet</Link>
+							{/* <ButtonGroup>
+								<Button
+									variant="outlined"
+									color="secondary"
+									disabled
+									sx={{
+										color: (theme) => `${theme.palette.text.primary}!important`,
+										background: (theme) => theme.customPalette.customBackground,
+										'&:hover': { background: (theme) => theme.customPalette.customBackground },
+									}}
+								>
+									Chain ID
+								</Button>
+								<Button
+									variant="outlined"
+									color="secondary"
+									endIcon={<IoOpenOutline size={14} />}
+									onClick={() => quarkNetworks && window.open(`${CHAIN_ID_URL}${quarkNetworks.chainId}`)}
+									sx={{
+										background: (theme) => theme.customPalette.customBackground,
+										'&:hover': { background: (theme) => theme.customPalette.customBackground },
+									}}
+								>
+									{quarkNetworks && obfuscateAddress(quarkNetworks.chainId)}
+								</Button>
+							</ButtonGroup> */}
+						</Grid>
+						<Grid item>
+							<ButtonGroup>
+								<Button
+									variant="outlined"
+									color="secondary"
+									disabled
+									sx={{
+										color: (theme) => `${theme.palette.text.primary}!important`,
+										background: (theme) => theme.customPalette.customBackground,
+										'&:hover': { background: (theme) => theme.customPalette.customBackground },
+									}}
+								>
+									Subnet ID
+								</Button>
+
+								<Button
+									variant="outlined"
+									color="secondary"
+									endIcon={<IoOpenOutline size={14} />}
+									onClick={() => window.open(`${SUBNET_ID_URL}${quarkNetworks.subnetId}`)}
+									sx={{
+										background: (theme) => theme.customPalette.customBackground,
+										'&:hover': { background: (theme) => theme.customPalette.customBackground },
+									}}
+								>
+									{obfuscateAddress(quarkNetworks.subnetId)}
+								</Button>
+							</ButtonGroup>
+						</Grid>
+					</Grid>
+				</Grid>
+			</Grid>
+		</Box>
+	)
+
+	return (
+		<Box pb={isMobile ? 6 : 4}>
+			<Divider sx={{ my: 4 }} />
 			<Grid container spacing={1} flexDirection="column" alignItems="center">
 				<Grid item container alignItems="center" justifyContent="center">
 					<Link
@@ -67,70 +190,66 @@ export const Footer = memo(() => {
 					</Link>
 				</Grid>
 				{quarkNetworks && (
-					<>
-						<Grid item container sx={{ borderRadius: 99999 }} alignItems="center" justifyContent="center">
-							<Grid item>
-								<ButtonGroup>
-									<Button
-										variant="outlined"
-										color="secondary"
-										disabled
-										sx={{
-											color: (theme) => `${theme.palette.text.primary}!important`,
-											background: (theme) => theme.customPalette.customBackground,
-											'&:hover': { background: (theme) => theme.customPalette.customBackground },
-										}}
-									>
-										Subnet ID
-									</Button>
+					<Grid container justifyContent="center" spacing={3}>
+						<Grid item>
+							<ButtonGroup>
+								<Button
+									variant="outlined"
+									color="secondary"
+									disabled
+									sx={{
+										color: (theme) => `${theme.palette.text.primary}!important`,
+										background: (theme) => theme.customPalette.customBackground,
+										'&:hover': { background: (theme) => theme.customPalette.customBackground },
+									}}
+								>
+									Subnet ID
+								</Button>
 
-									<Button
-										variant="outlined"
-										color="secondary"
-										endIcon={<IoOpenOutline size={14} />}
-										onClick={() => window.open(`${SUBNET_ID_URL}${quarkNetworks.subnetId}`)}
-										sx={{
-											background: (theme) => theme.customPalette.customBackground,
-											'&:hover': { background: (theme) => theme.customPalette.customBackground },
-										}}
-									>
-										{isMobile ? obfuscateAddress(quarkNetworks.subnetId) : quarkNetworks.subnetId}
-									</Button>
-								</ButtonGroup>
-							</Grid>
+								<Button
+									variant="outlined"
+									color="secondary"
+									endIcon={<IoOpenOutline size={14} />}
+									onClick={() => window.open(`${SUBNET_ID_URL}${quarkNetworks.subnetId}`)}
+									sx={{
+										background: (theme) => theme.customPalette.customBackground,
+										'&:hover': { background: (theme) => theme.customPalette.customBackground },
+									}}
+								>
+									{obfuscateAddress(quarkNetworks.subnetId)}
+								</Button>
+							</ButtonGroup>
 						</Grid>
-						<Grid item container sx={{ borderRadius: 99999 }} alignItems="center" justifyContent="center">
-							<Grid item>
-								<ButtonGroup>
-									<Button
-										variant="outlined"
-										color="secondary"
-										disabled
-										sx={{
-											color: (theme) => `${theme.palette.text.primary}!important`,
-											background: (theme) => theme.customPalette.customBackground,
-											'&:hover': { background: (theme) => theme.customPalette.customBackground },
-										}}
-									>
-										Chain ID
-									</Button>
+						<Grid item>
+							<ButtonGroup>
+								<Button
+									variant="outlined"
+									color="secondary"
+									disabled
+									sx={{
+										color: (theme) => `${theme.palette.text.primary}!important`,
+										background: (theme) => theme.customPalette.customBackground,
+										'&:hover': { background: (theme) => theme.customPalette.customBackground },
+									}}
+								>
+									Chain ID
+								</Button>
 
-									<Button
-										variant="outlined"
-										color="secondary"
-										endIcon={<IoOpenOutline size={14} />}
-										onClick={() => window.open(`${CHAIN_ID_URL}${quarkNetworks.chainId}`)}
-										sx={{
-											background: (theme) => theme.customPalette.customBackground,
-											'&:hover': { background: (theme) => theme.customPalette.customBackground },
-										}}
-									>
-										{isMobile ? obfuscateAddress(quarkNetworks.chainId) : quarkNetworks.chainId}
-									</Button>
-								</ButtonGroup>
-							</Grid>
+								<Button
+									variant="outlined"
+									color="secondary"
+									endIcon={<IoOpenOutline size={14} />}
+									onClick={() => window.open(`${CHAIN_ID_URL}${quarkNetworks.chainId}`)}
+									sx={{
+										background: (theme) => theme.customPalette.customBackground,
+										'&:hover': { background: (theme) => theme.customPalette.customBackground },
+									}}
+								>
+									{obfuscateAddress(quarkNetworks.chainId)}
+								</Button>
+							</ButtonGroup>
 						</Grid>
-					</>
+					</Grid>
 				)}
 				<Grid item>
 					<Typography component="footer" variant="body2" sx={{ width: '100%', opacity: 0.5, py: 1 }} align="center">
